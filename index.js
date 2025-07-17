d24dda52bf553230787fd9b41b1f349bcf85f67a
Emin Basbayan <emin.basbayan@icloud.com>
2025-07-17 22:35:58 +0300

    ecmascript module yapısı

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-console.log("hello")
+import toplamaYap from './topla.js';
+import { bolmeYap, carpmaYap } from './topla.js';
+
+
+
+console.log(toplamaYap(4, 5));
---
+++ topla.js
@@ -0,0 +1,13 @@
+const toplamaYap = (a, b) => {
+  return a + b;
+};
+
+export const bolmeYap = (a, b)=> {
+    return a / b;
+}
+
+export const carpmaYap = (a, b)=> {
+    return a * b;
+}
+
+export default toplamaYap;
