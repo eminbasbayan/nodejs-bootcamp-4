1b200ec1c2b71a911345d596a8ec6feb8d204f23
Emin Basbayan <emin.basbayan@icloud.com>
2025-07-17 23:41:43 +0300

    path module

--- index.js
+++ index.js
@@ -1,3 +1,27 @@
-const data = require("./data.json");
+const path = require('path');
 
-console.log(data.name);
+// Birden fazla klasör ismini birleştirip düzgün bir dosya yolu oluşturur.
+const yol = path.join('users', 'avatars', 'avatar.png');
+console.log('join: ', yol);
+
+// Mutlak (absolute) dosya yolunu üretir.
+const tamYol = path.resolve('users', 'avatars', 'avatar.png');
+console.log('resolve: ', tamYol);
+
+// basename - Dosya yolundan sadece dosya adını alır
+const dosyaAdi = path.basename("users/avatars/avatar.png")
+console.log(dosyaAdi);
+
+const klasorYolu = path.dirname("users/avatars/avatar.png");
+console.log(klasorYolu);
+
+const uzanti = path.extname(dosyaAdi)
+console.log(uzanti);
+
+console.log(__dirname);
+console.log(__filename);
+
+
+
+
+
