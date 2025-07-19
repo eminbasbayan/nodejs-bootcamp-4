42d0f49c3c7124f7933839dbd9ee68c3f97fd17e
Emin Basbayan <emin.basbayan@icloud.com>
2025-07-19 23:34:31 +0300

    http modülü

--- index.js
+++ index.js
@@ -1,87 +1,31 @@
-const fs = require('node:fs');
-const fsPromise = require("node:fs/promises")
+const http = require('http');
+const path = require('path');
+const fs = require('fs');
 
-// Dosya Yazma İşlemi
-// fs.writeFile('fatura.txt', 'Merhaba async dünya!', (err) => {
-//   if (err) throw err;
+const server = http.createServer((request, response) => {
+  let dosyaYolu = '';
 
-//   console.log('Dosya oluşturuldu ve yazıldı.');
-// });
+  if (request.url === '/') {
+    response.writeHead(200, { 'Content-Type': 'text/plain' });
+    response.end('Hoş geldin!');
+  } else if (request.url === '/hakkinda') {
+    dosyaYolu = path.join(__dirname, 'public', 'hakkinda.html');
+  } else {
+    response.writeHead(404, { 'Content-Type': 'text/plain' });
+    response.end('Böyle bir sayfa yok!');
+  }
 
-// try {
-//   fs.writeFileSync('dosya-sync.txt', 'Merhaba sync dünya!');
-// } catch (error) {
-//   console.log(error);
-// }
+  fs.readFile(dosyaYolu, (err, data) => {
+    if (err) {
+      response.writeHead(500, { 'Content-Type': 'text/plain' });
+      return response.end("Sunucuda bir hata oluştu");
+    }
 
-// fs.appendFile('dosya.txt', '\nYeni satır eklendi (async)', (err) => {
-//   if (err) throw err;
+    // response.writeHead(200, { 'Content-Type': 'text/html' });
+    response.end(data)
+  });
+});
 
-//   console.log('Veri eklendi.');
-// });
-
-// try {
-//   fs.appendFileSync('dosya-sync.txt', '\nYeni satır eklendi (sync)');
-//   console.log('Sync veri eklendi!');
-// } catch (error) {
-//   console.log(error);
-// }
-
-// fs.readFile('dosya.txt', 'utf8', (err, data) => {
-//   if (err) throw err;
-
-//   console.log('Dosya içeriği:', data);
-// });
-
-// try {
-//   const icerik = fs.readFileSync('dosya.txt', 'utf8');
-//   console.log('Sync içeriği:', icerik);
-// } catch (err) {
-//   console.error(err);
-// }
-
-// fs.unlink('new.txt', (err) => {
-//   if (err) throw err;
-//   console.log('Dosya silindi.');
-// });
-
-// try {
-//   fs.unlinkSync('new-sync.txt');
-//   console.log('Dosya sync silindi.');
-// } catch (err) {
-//   console.error(err);
-// }
-
-// fs.rename('dosya.txt', 'yeni.txt', (err) => {
-//   if (err) throw err;
-//   console.log('Dosya adı değiştirildi.');
-// });
-
-// fs.mkdir('yeniklasor', (err) => {
-//   if (err) throw err;
-//   console.log('Klasör oluşturuldu.');
-// });
-
-// if (fs.existsSync('dosya-sync.txt')) {
-//   console.log('Dosya mevcut.');
-// } else {
-//   console.log('Dosya bulunamadı.');
-// }
-
-// fs.stat('dosya-sync.txt', (err, stats) => {
-//   if (err) throw err;
-//   console.log('Bilgi:', stats);
-// });
-
-// fs.readdir('./', (err, dosyalar) => {
-//   if (err) throw err;
-//   console.log('Dosyalar:', dosyalar);
-// });
-
-
-// try {
-//   await fsPromise.writeFile('dosya.txt', 'Merhaba async/await!');
-//   console.log('Dosya oluşturuldu ve yazıldı.');
-// } catch (err) {
-//   console.error('Yazma hatası:', err);
-// }
+server.listen(5001, () => {
+  console.log('Server running on port 5001');
+});
