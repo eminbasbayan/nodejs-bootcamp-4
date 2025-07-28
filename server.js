93cd464bda1eae291ed172a8ad4566eb5cc5ba5f
BekirArdaBorcek <ardaborcekk@gmail.com>
2025-07-28 22:10:53 +0300

    middleware kullanım

--- server.js
+++ server.js
@@ -1,32 +1,51 @@
-const express = require('express');
+// Express ve Morgan modüllerini dahil ediyoruz
+const express = require("express");
+const morgan = require("morgan");
 const app = express();
 const port = 3000;
 
+//  NPM üzerindeki Log Middleware Kullanım Örneği
+app.use(morgan("dev"));
+
+// Middleware İşlenme Sırası
+app.use((req, res, next) => {
+  console.log("Middleware 1 çalıştı");
+  next();
+});
+
+app.use((req, res, next) => {
+  console.log("Middleware 2 çalıştı");
+  next();
+});
 
 // Veri alımı için GET metodu kullanılıyor
-app.get('/user', (req, res) => {
-  res.send('Hoş geldin!');
+app.get("/user", (req, res) => {
+  res.send("Hoş geldin!");
 });
 
 // User detaylarını almak için GET metodu kullanılıyor
-app.get('/user/:username', (req, res) => {
+app.get("/user/:username", (req, res) => {
   res.send(`User details for ${req.params.username}`);
 });
 
 // Yeni bir user oluşturma işlemi için POST metodu kullanılıyor
 app.post("/user", (req, res) => {
-  res.send('User created');
+  res.send("User created");
 });
 
-
 // Güncelleme işlemi için PUT metodu kullanılıyor
-app.put('/user/:username', (req, res) => {
+app.put("/user/:username", (req, res) => {
   res.send(`User with ID ${req.params.username} updated`);
 });
 
 // Silme işlemi için DELETE metodu kullanılıyor
-app.delete('/user/:username', (req, res) => {
+app.delete("/user/:username", (req, res) => {
   res.send(`User with ID ${req.params.username} deleted`);
+});
+
+// 404 middleware: Tanımlı olmayan route'lar için
+app.use((req, res, next) => {
+  res.status(404).send("404 - Sayfa bulunamadı");
 });
 
 app.listen(port, () => {
